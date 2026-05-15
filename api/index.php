5ca7381cabd80a1b0fbcf0b4668db7662afbd165
Maharani <mhrn310106@gmail.com>
2026-05-16 00:13:02 +0700

    fix laravel cache paths on vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
 
+$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+
 require __DIR__ . '/../public/index.php';
